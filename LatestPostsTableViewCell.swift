fdb7707bc4ddfdac034fdc492099e46be698f694
David <Dabustla@davids-imac.local>
2016-04-23 10:37:03 -0400

    Added View Controllers

---
+++ LatestPostsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  LatestPostsTableViewCell.swift
+//  Latest Posts
+//
+//  Created by David on 4/23/16.
+//  Copyright © 2016 RotoSports Journal. All rights reserved.
+//
+
+import UIKit
+
+class LatestPostsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
